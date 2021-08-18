fc90c600b74ac9a59ebb34b76ef004757fbc2ae1
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-08-18 23:45:13 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark bg-primary">
+			<div className="container">
+				<a href="#" className="navbar-brand">
+					Contacts
+				</a>
+				<div>
+					<a href="#" className="btn btn-light ml-auto"></a>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
